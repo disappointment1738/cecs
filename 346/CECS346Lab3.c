fda5f62c9ffca16d3914eeabbe6ab46fe8e51ee4
steph lee <40439815+disappointment1738@users.noreply.github.com>
2024-09-20 17:01:24 -0700

    Define port addresses
    uncommented todos and implemented constants definitions for enum my_states.
    
    TODO: address definition for LIGHT and SENSOR

--- 346/CECS346Lab3.c
+++ 346/CECS346Lab3.c
@@ -1,6 +1,7 @@
 // CECS346 Lab 3: FSM
-// Team members:?
-// Lab description:?
+// group 2
+// Team members: Stephanie Lee, Kayla Watt, & Daniel Espinosa
+// Lab description: ?
 
 // Hardware Design
 // 1)	Port E will be used to control 4 LEDs: white(PE3), red (PE2), yellow (PE1), green (PE0).
@@ -11,27 +12,27 @@
 
 // Registers for switches
 // Complete the following register definitions
-//#define SENSOR									  // bit addresses for the two switches/Sensors: bits 2&3
-//#define GPIO_PORTA_DATA_R       
-//#define GPIO_PORTA_DIR_R        
-//#define GPIO_PORTA_AFSEL_R      
-//#define GPIO_PORTA_DEN_R        
-//#define GPIO_PORTA_AMSEL_R      
-//#define GPIO_PORTA_PCTL_R       
-//#define GPIO_PORTA_PDR_R        
+#define SENSOR									  // bit addresses for the two switches/Sensors: bits 2&3
+#define GPIO_PORTA_DATA_R       (*((volatile unsigned long *)0x400043FC))
+#define GPIO_PORTA_DIR_R        (*((volatile unsigned long *)0x40004400))
+#define GPIO_PORTA_AFSEL_R      (*((volatile unsigned long *)0x40004420))
+#define GPIO_PORTA_DEN_R        (*((volatile unsigned long *)0x4000451C))
+#define GPIO_PORTA_AMSEL_R      (*((volatile unsigned long *)0x40004528))
+#define GPIO_PORTA_PCTL_R       (*((volatile unsigned long *)0x4000452C))
+#define GPIO_PORTA_PDR_R        (*((volatile unsigned long *)0x40004514))
 
 //// Registers for LEDs
-//#define LIGHT                   // bit addresses for the four LEDs
-//#define GPIO_PORTE_DIR_R        
-//#define GPIO_PORTE_AFSEL_R      
-//#define GPIO_PORTE_DEN_R        
-//#define GPIO_PORTE_AMSEL_R      
-//#define GPIO_PORTE_PCTL_R       
-//#define SYSCTL_RCGC2_R          
+#define LIGHT                   // bit addresses for the four LEDs
+#define GPIO_PORTE_DIR_R        (*((volatile unsigned long *)0x40024400))
+#define GPIO_PORTE_AFSEL_R      (*((volatile unsigned long *)0x40024420))
+#define GPIO_PORTE_DEN_R        (*((volatile unsigned long *)0x4002451C))
+#define GPIO_PORTE_AMSEL_R      (*((volatile unsigned long *)0x40024528))
+#define GPIO_PORTE_PCTL_R       (*((volatile unsigned long *)0x4002452C))
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
 	
 // Constants definitions
-//#define SYSCTL_RCGC2_GPIOE        // port E Clock Gating Control
-//#define SYSCTL_RCGC2_GPIOA        // port A Clock Gating Control
+#define SYSCTL_RCGC2_GPIOE      0x00000010  // port E Clock Gating Control
+#define SYSCTL_RCGC2_GPIOA      0x00000001  // port A Clock Gating Control
 #define HALF_SEC  (0)   							// TODO: define the constant for generating 0.5 second delay
 
 void Delay(uint8_t n);
@@ -48,12 +49,12 @@ struct State {
 typedef const struct State STyp;
 
 // Constants definitions
-//enum my_states {};
+enum my_states {goN, waitN, goE, waitE};
 
 // Output pins are:3(white), 2(red), 1(yellow), 0(green)
 // Input pins are: 1:sw2, 0:sw1 
-// STyp FSM[]={
-//             };    
+STyp FSM[]={
+            };    
 
 int main(void){ 
   uint8_t S;  // index to the current state 
@@ -61,7 +62,7 @@ int main(void){
 	
 	Light_Init();
 	Sensor_Init();
-//  S = ?;                     // FSM start with green  
+  S = ?;                     // FSM start with green  
     
   while(1){
 		// Put your FSM engine here
@@ -78,24 +79,24 @@ void Delay(uint8_t n){
 }
 
 void Sensor_Init(void){
-//	SYSCTL_RCGC2_R |=       // Activate Port A clocks
-//	while (?){} // wait for clock to be active
-//		
-//	GPIO_PORTA_AMSEL_R  // Disable analog function on PA3-2
-//  GPIO_PORTA_PCTL_R  // Enable regular GPIO
-//  GPIO_PORTA_DIR_R     // Inputs on PA3-2
-//  GPIO_PORTA_AFSEL_R  // Regular function on PA3-2
-//  GPIO_PORTA_DEN_R     // Enable digital signals on PA3-2
-	//	GPIO_PORTA_PDR_R     // Optional: Enable pull-down resistors for PA3-2
+	SYSCTL_RCGC2_R |=       // Activate Port A clocks
+	while (?){} // wait for clock to be active
+		
+	GPIO_PORTA_AMSEL_R  // Disable analog function on PA3-2
+  GPIO_PORTA_PCTL_R  // Enable regular GPIO
+  GPIO_PORTA_DIR_R     // Inputs on PA3-2
+  GPIO_PORTA_AFSEL_R  // Regular function on PA3-2
+  GPIO_PORTA_DEN_R     // Enable digital signals on PA3-2
+	GPIO_PORTA_PDR_R     // Optional: Enable pull-down resistors for PA3-2
 }
 
 void Light_Init(void){
-//	SYSCTL_RCGC2_R |=       // Activate Port E clocks
-//	while (?){} // wait for clock to be active
-//		
-//  GPIO_PORTE_AMSEL_R  // Disable analog function on PE3-0
-//  GPIO_PORTE_PCTL_R  // Enable regular GPIO
-//  GPIO_PORTE_DIR_R     // Outputs on PE3-0
-//  GPIO_PORTE_AFSEL_R  // Regular function on PE3-0
-//  GPIO_PORTE_DEN_R     // Enable digital on PE3-0
+	SYSCTL_RCGC2_R |=       // Activate Port E clocks
+	while (?){} // wait for clock to be active
+		
+  GPIO_PORTE_AMSEL_R  // Disable analog function on PE3-0
+  GPIO_PORTE_PCTL_R  // Enable regular GPIO
+  GPIO_PORTE_DIR_R     // Outputs on PE3-0
+  GPIO_PORTE_AFSEL_R  // Regular function on PE3-0
+  GPIO_PORTE_DEN_R     // Enable digital on PE3-0
 }
